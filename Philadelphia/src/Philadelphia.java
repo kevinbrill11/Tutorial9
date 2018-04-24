9db2899851b4e1350bbb2d5e1ed9cb4ec4fa192b
Kevin Brill <kevinjbrill@utexas.edu>
2018-04-24 18:52:00 -0500

    initial commit

---
+++ Philadelphia/src/Philadelphia.java
@@ -0,0 +1,5 @@
+public class Philadelphia {
+    public static boolean isitSunny(){
+        return true;
+    }
+}
---
+++ Philadelphia/src/PhiladelphiaTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+public class PhiladelphiaTest{
+    @Test
+    public void testIsItSunny(){
+        assertTrue(Philadelphia.isitSunny());
+    }
+
+
+    @Test
+    void isitSunny() {
+    }
+}
